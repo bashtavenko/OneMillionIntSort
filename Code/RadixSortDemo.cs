bf3401d9ce4ee1141ce6884ca03d029f97c15e91
Stan B <stanbpublic@live.com>
2014-09-11 19:25:13 -0400

    Initial commit

---
+++ Code/RadixSortDemo.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code
+{
+    public class RadixSortDemo
+    {
+      public static int[] KeyIndexCountingSort(int[] a, int r)
+      {
+        int n = a.Length;
+        int[] count = new int[r + 1];
+        int[] aux = new int[n];
+
+        // 1. Compute frequencies counts
+        for (int i = 0; i < n; i++)
+        {
+          count[a[i]+1]++;
+        }
+
+        // 2. Transform counts to indices
+        for (int j = 0; j < r; j++)
+        {
+          count[j + 1] += count[j];
+        }
+
+        // 3. Distribute the data
+        for (int i = 0; i < n; i++)
+        {
+          aux[count[a[i]]++] = a[i];
+        }
+
+        // 4. Copy back
+        for (int i = 0; i < n; i++)
+        {
+          a[i] = aux[i];
+        }
+
+        return a;
+      }
+
+      public static void LsdSort(string[] a, int w)
+      {
+        const int radix = 256;
+        int n = a.Length;
+        string[] aux = new string[n];
+
+        for (int d = w - 1; d >= 0; d--)
+        {
+          int[] count = new int[radix + 1];
+          for (int i = 0; i < n; i++)
+          {            
+            count[a[i][d] + 1]++;
+          }
+          for (int r = 0; r < radix; r++)
+          {
+            count[r + 1] += count[r];
+          }
+          for (int i = 0; i < n; i++)
+          {            
+            aux[count[a[i][d]]++] = a[i];
+          }
+          for (int i = 0; i < n; i++)
+          {
+            a[i] = aux[i];
+          }
+        }
+      }
+
+      public static void LsdIntSort(int[] a, int w)
+      {
+        int bits = w / 4;
+        int radix = (int)Math.Pow(2, bits);
+        int mask = radix - 1;
+        int n = a.Length;
+        int[] aux = new int[n];
+                
+        int digit;
+        for (int d = 0; d < w; d+=bits)
+        {          
+          int[] count = new int[radix + 1];
+          for (int i = 0; i < n; i++)
+          {
+            digit = (a[i] >> d) & mask;
+            count[digit + 1]++;
+          }
+          for (int r = 0; r < radix; r++)
+          {
+            count[r + 1] += count[r];
+          }
+          for (int i = 0; i < n; i++)
+          {
+            digit = (a[i] >> d) & mask;
+            aux[count[digit]++] = a[i];
+          }
+          for (int i = 0; i < n; i++)
+          {
+            a[i] = aux[i];
+          }
+        }
+      }
+    }
+}
---
+++ UnitTests/RadixSortDemoTest.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using Code;
+
+
+namespace UnitTests
+{
+  [TestClass]
+  public class RadixSortDemoTest
+  {
+    private int[] _input;
+    private string[] _inputString;
+
+    [TestInitialize]
+    public void Init()
+    {
+      _input = GenerateRandomArray(1000000);
+      _inputString = GenerateRandomStringArray(1000000);
+    }
+
+    [TestMethod]
+    public void KeyIndexCounting()
+    {
+      int[] input = FromCharToInt(new int[]{ 'd', 'a', 'c', 'f', 'f', 'b', 'd', 'b', 'f', 'b', 'e', 'a' });
+      int[] output = FromCharToInt(new int[] { 'a', 'a', 'b', 'b', 'b', 'c', 'd', 'd', 'e', 'f', 'f', 'f' });
+      int radix = 6;
+      int[] result = RadixSortDemo.KeyIndexCountingSort (input, radix);
+      CollectionAssert.AreEqual(result, output);
+    }
+
+    [TestMethod]
+    public void LsdRadixSort()
+    {
+      string[] input = new string[] {"dab",
+                                     "add", 
+                                     "cab", 
+                                     "fad", 
+                                     "fee", 
+                                     "bad", 
+                                     "dad",
+                                     "bee",
+                                     "fed",
+                                     "bed",
+                                     "ebb",
+                                     "ace" };
+      string[] output = new string[] {"ace",
+                                     "add", 
+                                     "bad", 
+                                     "bed", 
+                                     "bee", 
+                                     "cab", 
+                                     "dab",
+                                     "dad",
+                                     "ebb",
+                                     "fad",
+                                     "fed",
+                                     "fee" };
+      RadixSortDemo.LsdSort(input, 3);
+      CollectionAssert.AreEqual(input, output);
+    }
+
+    [TestMethod]
+    public void LstIntSortSimple()
+    {
+      int[] input = new int[] { 2, 8, 3, 1 };
+      int[] output = new int[] { 1, 2, 3, 8 };
+      RadixSortDemo.LsdIntSort(input, 4);
+      CollectionAssert.AreEqual(input, output);
+    }
+
+    [TestMethod]
+    public void LstIntSort10()
+    {
+      int[] input = GenerateRandomArray(10);      
+      RadixSortDemo.LsdIntSort(input, 32);
+      //Assert.IsTrue(IsSorted(input));
+    }
+
+    [TestMethod]
+    public void LstIntSortOneMillion()
+    {     
+      RadixSortDemo.LsdIntSort(_input, 32);
+      Assert.IsTrue(IsSorted(_input));
+    }
+
+    [TestMethod]
+    public void LstIntSortOneMillionArraySort()
+    {      
+      Array.Sort(_input);
+      //Assert.IsTrue(IsSorted(_input));
+    }
+
+    [TestMethod]
+    public void LstIntSortOneMillionAsStrings()
+    {        
+        RadixSortDemo.LsdSort(_inputString, 10);
+        //Assert.IsTrue(IsSorted(input));
+    }
+
+    private static int[] FromCharToInt(int[] a)
+    {
+      return a.Select(s => s - 97).ToArray();
+    }
+
+    private int[] GenerateRandomArray(int n)
+    {
+      int[] a = new int[n];
+      var r = new Random();
+
+      for (int i = 0; i < n; i++)
+      {
+        a[i] = r.Next();
+      }
+      return a;
+    }
+
+    private string[] GenerateRandomStringArray(int n)
+    {
+        string[] a = new string[n];
+        var r = new Random();
+
+        for (int i = 0; i < n; i++)
+        {
+            a[i] = r.Next().ToString("{0000000000}");
+        }
+        return a;
+    }
+
+    private bool IsSorted(int[] a)
+    {
+      for (int i = 1; i < a.Length; i++)
+      {
+        if (Less(a[i], a[i - 1])) return false;
+      }
+      return true;
+    }
+
+    private bool Less(int v, int w)
+    {
+      return v.CompareTo(w) < 0;
+    }
+
+  }
+}
